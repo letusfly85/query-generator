d2b712069c03af346481a378d2a0ecd8e5b86a96
letusfly85 <letusfly95@gmail.com>
2013-12-07 16:24:05 +0900

    add comments

--- src/main/groovy/com/jellyfish85/query/generator/constant/AppConst.groovy
+++ src/main/groovy/com/jellyfish85/query/generator/constant/AppConst.groovy
@@ -9,10 +9,13 @@ package com.jellyfish85.query.generator.constant
  */
 class AppConst {
 
+    // application group main system
     public final static String APPLICATION_GROUP_APP = "0"
 
+    // application group for interface
     public final static String APPLICATION_GROUP_IF  = "1"
 
+    // application group for translation
     public final static String APPLICATION_GROUP_IK  = "2"
 
 }
