0e7e44cad6ceacf5c21f0a072106c1c0f05db63e
letusfly85 <letusfly95@gmail.com>
2013-12-14 09:23:35 +0900

    remove unused importance and field

--- src/main/groovy/com/jellyfish85/query/generator/runner/GenerateErdReleaseScriptsRunner.groovy
+++ src/main/groovy/com/jellyfish85/query/generator/runner/GenerateErdReleaseScriptsRunner.groovy
@@ -1,17 +1,6 @@
 package com.jellyfish85.query.generator.runner
 
-import com.jellyfish85.dbaccessor.bean.erd.mainte.tool.MsTabColumnsBean
-import com.jellyfish85.dbaccessor.bean.erd.mainte.tool.MsTablesBean
-import com.jellyfish85.dbaccessor.bean.query.generate.tool.KrObjectDependenciesBean
-import com.jellyfish85.dbaccessor.dao.erd.mainte.tool.MsTabColumnsDao
-import com.jellyfish85.dbaccessor.dao.erd.mainte.tool.MsTablesDao
-import com.jellyfish85.dbaccessor.dao.query.generate.tool.KrObjectDependenciesDao
 import com.jellyfish85.query.generator.generator.ErdReleaseScriptsGenerator
-import com.jellyfish85.query.generator.generator.RenameQueryGenerator
-import com.jellyfish85.query.generator.generator.RestoreQueryGenerator
-import com.jellyfish85.query.generator.generator.TableDDLGenerator
-import com.jellyfish85.query.generator.helper.AppFileNameHelper
-import com.jellyfish85.query.generator.helper.TableNameHelper
 
 /**
  * == GenerateErdReleaseScriptsRunner ==
@@ -29,7 +18,6 @@ class GenerateErdReleaseScriptsRunner {
 
         def conn       = _context.getConnection()
         def argsHelper = _context.getArgsHelper()
-        AppFileNameHelper fileNameHelper = new AppFileNameHelper()
 
         String dependencyGrpCd          = args[0]
         ArrayList<String> tableNameList = argsHelper.requestTableNameList(args[1])
