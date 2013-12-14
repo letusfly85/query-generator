c3a62855d72308f69f820e83991075b784e75d0d
letusfly85 <letusfly95@gmail.com>
2013-12-15 00:12:02 +0900

    add trigger release scripts

---
+++ src/main/groovy/com/jellyfish85/query/generator/generator/TriggerReleaseScriptsGenerator.groovy
@@ -0,0 +1,26 @@
+package com.jellyfish85.query.generator.generator
+
+/**
+ * Generate triggers release DDLs and scripts
+ *
+ *
+ * @author wada shunsuke
+ * @since  2013/12/15
+ *
+ */
+class TriggerReleaseScriptsGenerator {
+
+    /**
+     * main method
+     *
+     *
+     * @author wada shunsuke
+     * @since  2013/12/15
+     *
+     */
+    public void generateTriggerReleaseScripts() {
+        //todo
+
+    }
+
+}
