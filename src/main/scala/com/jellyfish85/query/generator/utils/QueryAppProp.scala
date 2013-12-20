120287a0bcf9ec3dab92286f7d1739846f7afe94
letusfly85 <letusfly95@gmail.com>
2013-12-20 22:57:12 +0900

    remove comments

--- src/main/scala/com/jellyfish85/query/generator/utils/QueryAppProp.scala
+++ src/main/scala/com/jellyfish85/query/generator/utils/QueryAppProp.scala
@@ -4,29 +4,6 @@ import org.apache.commons.configuration.{PropertiesConfiguration, Configuration}
 
 class QueryAppProp {
 
-  /*
-  var inputStream: InputStream = _
-
-  var property: Properties     = new Properties()
-
-  var outputHome: String  = _
-
-  def setOutputHome {
-    if (System.getProperty("out.home") != null) {
-      this.outputHome = System.getProperty("out.home")
-
-    } else {
-      this.outputHome = "output"
-    }
-  }
-
-  def load {
-    setOutputHome
-    inputStream = getClass.getResourceAsStream("/com/jellyfish85/query/generator/define/query.properties")
-    property.load(inputStream)
-  }
-  */
-
   val configuration: Configuration =
     new PropertiesConfiguration("com/jellyfish85/query/generator/define/query.properties")
 
