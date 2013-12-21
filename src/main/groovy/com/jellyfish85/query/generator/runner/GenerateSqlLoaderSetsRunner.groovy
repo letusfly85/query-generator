3fb9e3440ee6aa4c08e5ad62010a5cd7b0cfb167
letusfly85 <letusfly95@gmail.com>
2013-12-21 16:57:38 +0900

    fix

--- src/main/groovy/com/jellyfish85/query/generator/runner/GenerateSqlLoaderSetsRunner.groovy
+++ src/main/groovy/com/jellyfish85/query/generator/runner/GenerateSqlLoaderSetsRunner.groovy
@@ -1,6 +1,11 @@
 package com.jellyfish85.query.generator.runner
 
+import com.jellyfish85.dbaccessor.bean.erd.mainte.tool.MsTabColumnsBean
+import com.jellyfish85.dbaccessor.bean.erd.mainte.tool.MsTablesBean
+import com.jellyfish85.dbaccessor.dao.erd.mainte.tool.MsTabColumnsDao
 import com.jellyfish85.query.generator.generator.GeneralCodeGenerator
+import com.jellyfish85.query.generator.helper.TableNameHelper
+import com.jellyfish85.xlsaccessor.utils.XlsAppProp
 
 /**
  * == GenerateSqlLoaderSetsRunner ==
@@ -14,9 +19,28 @@ import com.jellyfish85.query.generator.generator.GeneralCodeGenerator
 class GenerateSqlLoaderSetsRunner {
 
     public static void main(String[] args) {
+        BaseRunner _context = new BaseRunner()
+        _context.databaseInitialize()
+
+        def conn = _context.getConnection()
+
+        XlsAppProp      xlsAppProp      = new XlsAppProp()
+        String generalTableName = xlsAppProp.generalCodePhysicalTableName()
+
+        String dependencyGrpCd          = args[0]
+        TableNameHelper tableNameHelper = new TableNameHelper()
+        String schemaName = tableNameHelper.findByApplicationGroupCd(dependencyGrpCd, generalTableName)
+
+        MsTablesBean tablesBean = new MsTablesBean()
+        tablesBean.physicalTableNameAttr().setValue(generalTableName)
+
+        MsTabColumnsDao columnsDao = new MsTabColumnsDao()
+        def _columnList = columnsDao.find(conn, tablesBean)
+        ArrayList<MsTabColumnsBean> columnList = columnsDao.convert(_columnList)
 
         GeneralCodeGenerator generator = new GeneralCodeGenerator()
         generator.generateDataFile()
+        generator.generateControlFile(schemaName, columnList)
 
     }
 }
