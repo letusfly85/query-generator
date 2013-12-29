4a973de8fa4e765c7f0bdc3cfe9726244094009f
letusfly85 <letusfly95@gmail.com>
2013-12-30 00:39:03 +0900

    fix helper

--- src/main/groovy/com/jellyfish85/query/generator/helper/CodeGeneratorHelper.groovy
+++ src/main/groovy/com/jellyfish85/query/generator/helper/CodeGeneratorHelper.groovy
@@ -1,14 +1,41 @@
 package com.jellyfish85.query.generator.helper
 
+import com.jellyfish85.dbaccessor.bean.erd.mainte.tool.MsTablesBean
+import com.jellyfish85.query.generator.generator.GeneralGenerator
+
 /**
  * help generate code suite
  *
  *
  */
-class CodeGeneratorHelper {
+class CodeGeneratorHelper extends GeneralGenerator {
 
-    public void generateLoadingShellScript() {
+    /**
+     * generate sql loader script
+     *
+     * @author wada shunsuke
+     * @since  2013/12/30
+     * @param schemaName
+     * @param tablesBeans
+     * @param shellScriptPath
+     */
+    public void generateLoadingShellScript(
+            String schemaName,
+            ArrayList<MsTablesBean> tablesBeans,
+            String shellScriptPath
+    ) {
+        String tableName = tablesBeans.head().physicalTableNameAttr().value()
 
+        Map map = [
+                schemaName  : schemaName,
+                tableName   : tableName
+        ]
+
+        String path = "/com/jellyfish85/query/generator/template/shell/execSqlLoader.template"
+
+        this.generate(map, path)
+        this.setPath(shellScriptPath)
+        this.writeAppFile()
     }
 
 }
