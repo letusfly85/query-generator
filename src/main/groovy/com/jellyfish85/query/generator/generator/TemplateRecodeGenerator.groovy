03e20cbd35852d4354f1596c40e97b0bb54425b0
letusfly85 <letusfly95@gmail.com>
2014-01-01 12:55:31 +0900

    add template record generator

---
+++ src/main/groovy/com/jellyfish85/query/generator/generator/TemplateRecodeGenerator.groovy
@@ -0,0 +1,27 @@
+package com.jellyfish85.query.generator.generator
+
+/**
+ *
+ *
+ *
+ */
+class TemplateRecodeGenerator extends GeneralGenerator {
+
+    public void generateTemplateDeleteQuery() {
+
+    }
+
+    public void generateTemplateShellScript() {
+
+    }
+
+    public void generateTemplateControlFile() {
+
+    }
+
+
+    public void generateTemplateDataFile() {
+
+    }
+
+}
