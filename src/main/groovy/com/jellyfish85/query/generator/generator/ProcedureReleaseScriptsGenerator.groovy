182b9a64092774f41a23f2fc3a6dc792415580a4
letusfly85 <letusfly95@gmail.com>
2014-01-07 22:09:32 +0900

    fix encoding bugs 6

--- src/main/groovy/com/jellyfish85/query/generator/generator/ProcedureReleaseScriptsGenerator.groovy
+++ src/main/groovy/com/jellyfish85/query/generator/generator/ProcedureReleaseScriptsGenerator.groovy
@@ -36,8 +36,8 @@ class ProcedureReleaseScriptsGenerator extends GeneralGenerator {
             File src  = new File(this.context.queryProp.applicationWorkspacePath(), requestBean.path())
             File dest = new File(this.context.queryProp.procedureDDLFolder(), requestBean.fileName())
 
-            this.replaceUtils.convertSJIS2UTF8(src)
-            //this.replaceUtils.addSchemaName2Procedure(src, mainSchemaName, outerFaceSchemaName)
+            //this.replaceUtils.convertSJIS2UTF8(src)
+            this.replaceUtils.addSchemaName2Procedure(src, mainSchemaName, outerFaceSchemaName)
             FileUtils.copyFile(src, dest)
         }
     }
