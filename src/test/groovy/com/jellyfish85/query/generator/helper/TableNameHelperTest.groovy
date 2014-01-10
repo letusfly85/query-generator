10492ae51494470fa2962ffdbd435e2b74d90c11
letusfly85 <letusfly95@gmail.com>
2014-01-11 01:29:29 +0900

    modify request schema name helpers

--- src/test/groovy/com/jellyfish85/query/generator/helper/TableNameHelperTest.groovy
+++ src/test/groovy/com/jellyfish85/query/generator/helper/TableNameHelperTest.groovy
@@ -1,6 +1,10 @@
 package com.jellyfish85.query.generator.helper
 
+import com.jellyfish85.query.generator.constant.QueryAppConst
 import com.jellyfish85.query.generator.utils.QueryAppProp
+import com.jellyfish85.xlsaccessor.constant.AppConst
+
+import javax.management.Query
 
 /**
  * test class of TableNameHelper
@@ -20,6 +24,16 @@ class TableNameHelperTest extends GroovyTestCase {
     void testRequestBITableName() {
         String biTableName = tableNameHelper.requestBITableName("M_BI_TABLE")
         assertEquals("table name should be C_BI_TABLE", "C_BI_TABLE", biTableName)
+    }
+
+    void testRequestMainSchemaName() {
+        String mainSchemaName =
+                tableNameHelper.requestMainSchemaName(QueryAppConst.APPLICATION_MAIN_GROUP)
+        assertEquals("schema name should be main group schema", _queryProp.erdSchemaMainName(), mainSchemaName)
+
+        String subSchemaName =
+                tableNameHelper.requestMainSchemaName(QueryAppConst.APPLICATION_SUB_GROUP)
+        assertEquals("schema name should be sub group schema",  _queryProp.erdSchemaSubName(), subSchemaName)
     }
 
     /*
@@ -49,9 +63,7 @@ class TableNameHelperTest extends GroovyTestCase {
 
     }
 
-    void testRequestMainSchemaName() {
 
-    }
 
     void testRequestOuterFaceSchemaName() {
 
