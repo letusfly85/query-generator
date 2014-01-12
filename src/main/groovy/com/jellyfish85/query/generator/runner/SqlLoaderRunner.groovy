9edf3ea10e8e1f7457389bb68adc80a1ba41a5e3
letusfly85 <letusfly95@gmail.com>
2014-01-12 21:52:04 +0900

    add print stream option

--- src/main/groovy/com/jellyfish85/query/generator/runner/SqlLoaderRunner.groovy
+++ src/main/groovy/com/jellyfish85/query/generator/runner/SqlLoaderRunner.groovy
@@ -5,7 +5,6 @@ import org.apache.commons.io.FileUtils
 
 class SqlLoaderRunner {
 
-    /*
     public static void main(String[] args) {
 
         def dependencyGrpCd = args[0]
@@ -37,14 +36,43 @@ class SqlLoaderRunner {
         int result = process.waitFor()
 
         if (result.equals(0)) {
+            InputStream is = process.getInputStream()
+            BufferedReader br = new BufferedReader(new InputStreamReader(is))
+            try {
+                for (;;) {
+                    String line = br.readLine()
+                    if (line == null) break
+                    println(line)
+                }
+            } finally {
+                br.close();
+            }
             return
 
         } else {
+            InputStream is = process.getErrorStream()
+            print(is)
+            BufferedReader br = new BufferedReader(new InputStreamReader(is))
+            try {
+                for (;;) {
+                    String line = br.readLine()
+                    if (line == null) break;
+                    println(line)
+                }
+            } finally {
+                br.close()
+            }
             throw new RuntimeException()
         }
     }
-    */
 
+    /**
+     * generate executable bat file for sql loader on ms a machine
+     * and execute it
+     *
+     * @param runner
+     * @param scriptPath
+     */
     public void msWinExec(BaseRunner  runner, String scriptPath) {
         BaseContext context = runner._context
         def queryProp = context.queryProp
@@ -69,10 +97,29 @@ class SqlLoaderRunner {
         int result = process.waitFor()
 
         if (result.equals(0)) {
+            InputStream is = process.getInputStream()
+            printInputStream(is)
             return
 
         } else {
+            InputStream es = process.getErrorStream()
+            print(es)
+            printInputStream(es)
+
             throw new RuntimeException()
+        }
+    }
+
+    private void printInputStream(InputStream is){
+        BufferedReader br = new BufferedReader(new InputStreamReader(is))
+        try {
+            for (;;) {
+                String line = br.readLine()
+                if (line == null) break;
+                println(line)
+            }
+        } finally {
+            br.close()
         }
     }
 }
