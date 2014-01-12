1f363e43a958275df62e461751fc4fdee74d77d8
letusfly85 <letusfly95@gmail.com>
2014-01-12 15:47:17 +0900

    add dependencies set check test method

--- src/test/groovy/com/jellyfish85/query/generator/runner/BaseRunnerTest.groovy
+++ src/test/groovy/com/jellyfish85/query/generator/runner/BaseRunnerTest.groovy
@@ -1,9 +1,19 @@
 package com.jellyfish85.query.generator.runner
 
+import com.jellyfish85.dbaccessor.bean.query.generate.tool.KrObjectDependenciesBean
 import com.jellyfish85.query.generator.utils.QueryAppProp
+import org.dbunit.operation.DatabaseOperation
 
 import java.sql.Connection
 import java.sql.DatabaseMetaData
+
+import org.dbunit.database.DatabaseConnection
+import org.dbunit.database.IDatabaseConnection
+import org.dbunit.dataset.IDataSet
+import org.dbunit.dataset.excel.XlsDataSet
+//import org.dbunit.dataset.csv.CsvDataSetWriter
+//import org.dbunit.dataset.xml.FlatDtdDataSet
+//import org.dbunit.dataset.xml.FlatXmlDataSet
 
 /**
  *
@@ -12,14 +22,29 @@ import java.sql.DatabaseMetaData
  */
 class BaseRunnerTest extends GroovyTestCase {
 
-    String      dependentGrpCd = System.getProperty("4test.dependentGrpCd")
-    String     _environment    = System.getProperty("4test.environment")
-    BaseRunner runner          = new BaseRunner(dependentGrpCd, _environment)
-
-    QueryAppProp _queryProp = new QueryAppProp(_environment)
+    String        dependentGrpCd = null
+    String       _environment    = null
+    BaseRunner   runner          = null
+    QueryAppProp _queryProp      = null
 
     void setUp() {
+        dependentGrpCd = System.getProperty("4test.dependentGrpCd")
+        _environment    = System.getProperty("4test.environment")
+        runner          = new BaseRunner(dependentGrpCd, _environment)
+
+        _queryProp = new QueryAppProp(_environment)
+
         runner.databaseInitialize()
+
+        Connection conn = runner.getConnection()
+        IDatabaseConnection iConn = new DatabaseConnection(conn, _queryProp.erdSchema4Test())
+
+        String url = "/com/jellyfish85/query/generator/runner/KR_OBJECT_DEPENDENCIES.xls"
+        File file  = new File(getClass().getResource(url).toURI())
+        FileInputStream inputStream = new FileInputStream(file)
+        IDataSet partialDataSet     = new XlsDataSet(inputStream)
+
+        DatabaseOperation.CLEAN_INSERT.execute(iConn, partialDataSet)
     }
 
     void tearDown() {
@@ -27,19 +52,19 @@ class BaseRunnerTest extends GroovyTestCase {
     }
 
     void testGetConnection() {
-
         // check Connection commit mode
         Connection conn = runner.getConnection()
         assertEquals("auto commit should be true", conn.getAutoCommit(), false)
 
         // confirm test schema name
         DatabaseMetaData metaData = conn.getMetaData()
-        assertEquals("schemaName", metaData.getUserName(), _queryProp.erdSchema4Test())
+        assertEquals("schemaName should be test schema name",
+                metaData.getUserName(), _queryProp.erdSchema4Test())
     }
 
-    /*
     void testGetDependencies() {
-
+        // check dependencies list
+        ArrayList<KrObjectDependenciesBean> beans = runner.getDependencies()
+        assertEquals("beans size should be 3.", 3, beans.size())
     }
-    */
 }
