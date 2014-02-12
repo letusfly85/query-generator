7531be4ea356f079ae22c995efa2e5d86f315209
letusfly85 <letusfly95@gmail.com>
2014-02-12 20:54:16 +0900

    remove other system entities

--- src/main/groovy/com/jellyfish85/query/generator/runner/GenerateErdReleaseScriptsRunner.groovy
+++ src/main/groovy/com/jellyfish85/query/generator/runner/GenerateErdReleaseScriptsRunner.groovy
@@ -1,8 +1,10 @@
 package com.jellyfish85.query.generator.runner
 
+import com.jellyfish85.dbaccessor.bean.erd.mainte.tool.MsTabColumnsBean
 import com.jellyfish85.dbaccessor.bean.erd.mainte.tool.MsTablesBean
 import com.jellyfish85.dbaccessor.dao.erd.mainte.tool.MsTablesDao
 import com.jellyfish85.query.generator.BaseContext
+import com.jellyfish85.query.generator.constant.QueryAppConst
 import com.jellyfish85.query.generator.generator.ErdReleaseScriptsGenerator
 import com.jellyfish85.query.generator.helper.ResourceCopyHelper
 
@@ -36,7 +38,16 @@ class GenerateErdReleaseScriptsRunner {
         MsTablesDao msTablesDao = new MsTablesDao()
         def _tableList = msTablesDao.findByReleaseId(conn, preReleaseId)
         ArrayList<MsTablesBean> tableList = msTablesDao.convert(_tableList)
-        generator.generateErdReleaseScripts(conn, dependencies, tableList)
+        ArrayList<MsTablesBean> tables = new ArrayList<>()
+        tableList.findAll {MsTablesBean bean ->
+            def tableName =  bean.physicalTableNameAttr().value()
+            def group     =  context.tableNameHelper.identifyTableGroup(tableName)
+
+            if (group != QueryAppConst.APPLICATION_GROUP_BI) {
+                tables.add(bean)
+            }
+        }
+        generator.generateErdReleaseScripts(conn, dependencies, tables)
 
         // add login sql to parent folder
         ResourceCopyHelper copyHelper = new ResourceCopyHelper()
